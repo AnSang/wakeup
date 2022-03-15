f03df366c0af7fa2f7de186fe5103a877681a8c1
ansang <sho989434@naver.com>
2022-03-15 16:41:21 +0900

    알람 기록 객체 구성변경

--- lib/models/alarm_record.dart
+++ lib/models/alarm_record.dart
@@ -1,25 +1,25 @@
 class AlarmRecord {
-  final String date;
-  final String time;
-  final int count;
+  final DateTime start; // 수면 시작시간
+  final DateTime end;   // 수면 종료시간
+  final int count;      // 총 알람 횟수
 
   AlarmRecord({
-    required this.date,
-    required this.time,
+    required this.start,
+    required this.end,
     required this.count,
   });
 
   AlarmRecord.fromJson(Map<String, dynamic> json)
       : this(
-      date: json['date']! as String,
-      time: json['time']! as String,
+      start: json['start']! as DateTime,
+      end: json['end']! as DateTime,
       count: json['count']! as int
   );
 
   Map<String, dynamic> toJson() {
     return {
-      'date' : date,
-      'time' : time,
+      'start' : start,
+      'end' : end,
       'count' : count
     };
   }
