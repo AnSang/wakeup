3bf7c214a91cb0592065ed96246540f01d0bdc25
ansang-eun <sho989434@naver.com>
2022-04-01 12:43:20 +0900

    1. 수면기록 데이터 구조 변경

--- lib/models/alarm_record.dart
+++ lib/models/alarm_record.dart
@@ -1,26 +1,23 @@
 class AlarmRecord {
-  final DateTime start; // 수면 시작시간
-  final DateTime end;   // 수면 종료시간
-  final int count;      // 총 알람 횟수
+  final String date;  // 수면 날짜
+  final String time;  // 수면 시간
+  String? document;
 
   AlarmRecord({
-    required this.start,
-    required this.end,
-    required this.count,
+    required this.date,
+    required this.time
   });
 
   AlarmRecord.fromJson(Map<String, dynamic> json)
       : this(
-      start: json['start']! as DateTime,
-      end: json['end']! as DateTime,
-      count: json['count']! as int
+      date: json['date']! as String,
+      time: json['time']! as String
   );
 
   Map<String, dynamic> toJson() {
     return {
-      'start' : start,
-      'end' : end,
-      'count' : count
+      'date' : date,
+      'time' : time
     };
   }
 }
