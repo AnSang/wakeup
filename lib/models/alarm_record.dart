a290b9655663db8a747717d6497258a035de9cc5
ansang <sho989434@naver.com>
2022-04-08 15:57:35 +0900

    1. AlarmRecord 데이터 구조 변경

--- lib/models/alarm_record.dart
+++ lib/models/alarm_record.dart
@@ -1,23 +1,32 @@
 class AlarmRecord {
-  final String date;  // 수면 날짜
-  final String time;  // 수면 시간
-  String? document;
+  String sDate;   // 수면 시작날짜
+  String sTime;   // 수면 시작시간
+  String? eDate;   // 수면 종료날짜
+  String? eTime;   // 수면 시작시간
+
+  /// eDate, eTime 값은 나중에 설정해주기 때문에 Null 이 나타날수있다.
 
   AlarmRecord({
-    required this.date,
-    required this.time
+    required this.sDate,
+    required this.sTime,
+    required this.eDate,
+    required this.eTime,
   });
 
   AlarmRecord.fromJson(Map<String, dynamic> json)
       : this(
-      date: json['date']! as String,
-      time: json['time']! as String
+      sDate: json['sDate']!,
+      sTime: json['sTime']!,
+      eDate: json['eDate'],
+      eTime: json['eTime'],
   );
 
   Map<String, dynamic> toJson() {
     return {
-      'date' : date,
-      'time' : time
+      'sDate' : sDate,
+      'sTime' : sTime,
+      'eDate' : eDate,
+      'eTime' : eTime
     };
   }
 }
