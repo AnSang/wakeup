d1d0bab3384b0d55b96d7ba646e4c86a56d971c8
ansang <sho989434@naver.com>
2022-04-12 19:10:04 +0900

    1. 개인정보약관 화면 생성

---
+++ lib/ui/screen_terms.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/cupertino.dart';
+
+class InfoTerms extends StatelessWidget {
+  const InfoTerms({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+
+}
